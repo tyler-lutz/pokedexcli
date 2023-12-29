3ebcffd7b2946dfb38666e5e868bdc62831b19c0
= <66930763+tyler-lutz@users.noreply.github.com>
2023-12-29 13:03:45 -0500

    refactor: move client struct and newClient() to client.go

---
+++ internal/pokeapi/client.go
@@ -0,0 +1,18 @@
+package pokeapi
+
+import (
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	httpClient http.Client
+}
+
+func NewClient() Client {
+	return Client{
+		httpClient: http.Client{
+			Timeout: time.Minute,
+		},
+	}
+}
--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -4,23 +4,9 @@ import (
 	"encoding/json"
 	"errors"
 	"io"
-	"net/http"
-	"time"
 )
 
 const baseURL string = "https://pokeapi.co/api/v2"
-
-type Client struct {
-	httpClient http.Client
-}
-
-func NewClient() Client {
-	return Client{
-		httpClient: http.Client{
-			Timeout: time.Minute,
-		},
-	}
-}
 
 // LocationArea is a location area in the pokemon world.
 type LocationAreasResponse struct {
